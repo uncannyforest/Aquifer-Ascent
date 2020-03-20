bb538d43a4739999c92af1cf93f3d16aa06bea62
Annie <annekalford@gmail.com>
2020-03-20 13:36:59 -0400

    Add glow materials, a pick-up script, and a Clickable tag

---
+++ Assets/Scripts/PickMeUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickMeUp : MonoBehaviour
+{
+    private Rigidbody rb;
+    private GameObject player;
+    Collider m_Collider;
+    bool pickedUp = false;
+
+    void Start(){
+        player = GameObject.FindWithTag("Player");
+        //Fetch the GameObject's Collider (make sure it has a Collider component)
+        m_Collider = GetComponent<Collider>();
+    }
+
+
+    // Update is called once per frame
+    void Update(){
+    /*  if (Input.GetButton("pickup"))
+        { // Define it in the input manager
+            // do something... 
+        }
+    */
+    }
+
+    void OnMouseDown(){
+        if (!pickedUp){
+            PickUp();
+        }
+    }
+
+
+    private void PickUp(){
+        // Sets "newParent" as the new parent of the child GameObject.
+        Debug.Log("You clicked me!");
+        this.transform.SetParent(player.transform);
+        this.transform.position = player.transform.Find("HoldLocation").transform.position;
+        m_Collider.enabled = false;
+        pickedUp = true;
+    }
+
+
+}
