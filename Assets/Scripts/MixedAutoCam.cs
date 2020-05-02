0c366d3519858c885cf492dece14abeea0ce4a39
Uncanny Forest <git@uncannyforest.com>
2020-05-02 19:32:12 -0400

    Don't move camera when walking towards it

--- Assets/Scripts/MixedAutoCam.cs
+++ Assets/Scripts/MixedAutoCam.cs
@@ -16,6 +16,7 @@ namespace UnityStandardAssets.Cameras
         [SerializeField] private bool m_FollowTilt = true; // Whether the rig will tilt (around X axis) with the target.
         [SerializeField] private float m_TargetVelocityLowerLimit = 4f;// the minimum velocity above which the camera turns towards the object's velocity.
         [SerializeField] private float m_SmoothTurnTime = 0.2f; // the smoothing for the camera's rotation
+        [SerializeField] private float m_WalkBackwardsAngle = 20; // Angle to not move camera when facing the camera
         [SerializeField] private float m_ResetTurnSpeed = 8; // How fast the rig will turn when resetting
 
         private float m_LastFlatAngle; // The relative angle of the target and the rig from the previous frame.
@@ -84,6 +85,9 @@ namespace UnityStandardAssets.Cameras
 
             // and aligning with the target object's up direction (i.e. its 'roll')
             m_RollUp = m_RollSpeed > 0 ? Vector3.Slerp(m_RollUp, targetUp, m_RollSpeed*deltaTime) : Vector3.up;
+            if(!isResettingCamera && Quaternion.Angle(transform.rotation, rollRotation) > 180 - m_WalkBackwardsAngle) {
+                return; // Don't adjust angle, walking backwards                
+            }
             float turnSpeedToUse = isResettingCamera ? m_ResetTurnSpeed : m_TurnSpeed;
             transform.rotation = Quaternion.Lerp(transform.rotation, rollRotation, turnSpeedToUse*m_CurrentTurnAmount*deltaTime);
 
