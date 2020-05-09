0c264da1aa5dfbbec80ce90072e6b55b32c47d6e
Uncanny Forest <git@uncannyforest.com>
2020-05-09 00:02:12 -0400

    Orb movement use velocity not position

--- Assets/Scripts/FloatWanderAI.cs
+++ Assets/Scripts/FloatWanderAI.cs
@@ -2,30 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class FloatWanderAI : MonoBehaviour
 {
-    public float moveSpeed = 0.1f;
+    public float turnSpeed = 0.02f;
+    public float maxMoveSpeed = 0.05f;
     public float updateTime = 0.5f;
 
     private bool alreadyMoving = false;
-    private Vector3 direction = new Vector3(0, 0, 0);
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    Rigidbody rigidBody;
 
+    void Start() {
+        rigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate() {
         if (canMove()) {
             if (alreadyMoving == false) {
                 StartCoroutine(Wander());
             }
-        }
-        if (canMove()) {
-            transform.position += direction * moveSpeed * Time.deltaTime;
         }
     }
 
@@ -43,9 +39,9 @@ public class FloatWanderAI : MonoBehaviour
         float x = Mathf.Sqrt(1 - y * y) * Mathf.Cos(theta);
         float z = Mathf.Sqrt(1 - y * y) * Mathf.Sin(theta);
 
-        direction += new Vector3(x, y, z);
-        if (direction.magnitude > 1f) {
-            direction /= direction.magnitude;
+        rigidBody.velocity += new Vector3(x, y, z) * turnSpeed;
+        if (rigidBody.velocity.magnitude > maxMoveSpeed) {
+            rigidBody.velocity = rigidBody.velocity.normalized * maxMoveSpeed;
         }
     }
 
