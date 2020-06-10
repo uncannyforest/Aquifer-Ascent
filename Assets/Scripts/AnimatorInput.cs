fc9c0f04d05167bffdb7c7194ba80ae6d5a9d1ea
Uncanny Forest <git@uncannyforest.com>
2020-06-09 20:29:23 -0400

    Flower only turns off light when all closed

---
+++ Assets/Scripts/AnimatorInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorInput : BooleanScript {
+    public Animator sendingAnimator;
+    public int layerIndex;
+    public string animatorState;
+
+    override public bool IsActive {
+        get {
+            AnimatorStateInfo stateInfo = sendingAnimator.GetCurrentAnimatorStateInfo(layerIndex);
+            return stateInfo.IsName(animatorState) && !sendingAnimator.IsInTransition(layerIndex);
+        }
+    }
+}
